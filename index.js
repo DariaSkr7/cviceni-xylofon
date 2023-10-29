ebaa8de269f9d8cdf009b38a448f371a37ac158f
Daria Skoryk <dariaskoryk@gmail.com>
2023-10-29 17:13:55 +0100

    result

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Funguju')
+console.log('Funguju');
+
+const header = document.querySelector('h1');
+
+const kameny = document.querySelectorAll('.kamen');
+kameny.forEach((kamen) => {
+  kamen.addEventListener('click', () => {
+    header.textContent = kamen.textContent;
+    const zvuk = new Audio(`tony/${kamen.textContent}.mp3`);
+    zvuk.play();
+  });
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.code.includes('Digit')) {
+    const cislo = Number(event.code.slice(5));
+    const zvuk = new Audio(`tony/${kameny[cislo].textContent}.mp3`);
+    zvuk.play();
+  }
+});
